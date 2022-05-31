98e36c6b72339ffbba5af3aaafc9efd027a1c09d
Charles O'Brien <charlesobriennb@gmail.com>
2022-05-31 18:35:14 -0300

    Very simple PHP login
    A very simple login system written in PHP and using Twitter Bootstrap. A bare bones system that can be expanded upon as needed.

---
+++ authentication.php
@@ -0,0 +1,27 @@
+<?php      
+    include('connection.php');  
+/** @var type $username */
+$username = $_POST['user'];  
+/** @var type $password */
+$password = $_POST['pass'];
+
+//
+
+      
+        //Prevent SQL Injection
+
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        }      
+?>  
